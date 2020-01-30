d5ebe98eac80be7ad4b0d1103330076eab637783
r.vincent <robertus.vincent@campus.tu-berlin.de>
2020-01-30 23:05:21 +0100

    Added post genre and movie. TODO: pre-format the list of movie and list of cinema/showtime output

--- api_webapp/flaskr/api/mobile.py
+++ api_webapp/flaskr/api/mobile.py
@@ -3,18 +3,18 @@ import datetime
 from flask import Blueprint, request, session
 
 bp_mobile_api = Blueprint('mobile', __name__, url_prefix='/api/mobile')
-cinema_api_key = "EDX4mJjrvtFwWe0ZvrZ9NPwRITLTdjuA"
-
 
 @bp_mobile_api.route('/movies', methods=['POST'])
-def movies():
+def get_movies():
     if request.is_json:
         json_input = request.get_json()
         print(json_input)
         genre_preferences = json_input["GenrePreferences"]
         most_voted_genre = max(genre_preferences, key=lambda key: genre_preferences[key])
-        print(most_voted_genre)
-        most_voted_genre_id = 0
+        list_of_genres = _fetch_genres()
+        genre_obj = next((item for item in list_of_genres['genres'] if item['name'] == most_voted_genre), None)
+        print(genre_obj["id"])
+        genre_id = genre_obj["id"]
 
         lat = json_input["UserLat"]
         lon = json_input["UserLon"]
@@ -22,24 +22,46 @@ def movies():
         # Step 2.2x
         movie_request = 'http://localhost:5000/api/cinemas/movies?location={},{}&distance=5&genre_ids={}'.format(lat,
                                                                                                                  lon,
-                                                                                                                 most_voted_genre_id)
+                                                                                                                 genre_id)
 
         r = requests.get(movie_request)
         r_json = r.json()
         print(r_json)
 
-        list_of_genres = _fetch_genres()
-        genre_obj = next((item for item in list_of_genres['genres'] if item['name'] == most_voted_genre), None)
-        print(genre_obj)
+        # TODO: format the output
+
+
         headers = {"Content-type": "application/json"}
         return json_input
     else:
         error = "An error has occurred: Invalid JSON input. Error code: {}".format(500)
         return error
 
+@bp_mobile_api.route('/cinemas', methods=['POST'])
+def get_cinemas():
+    if request.is_json:
+        json_input = request.get_json()
+        print(json_input)
+
+        lat = json_input["UserLat"]
+        lon = json_input["UserLon"]
+        movie_id = json_input["MovieId"]
+        time_from = json_input["Date"]
+
+        showtime_request = 'http://127.0.0.1:5000/api/cinemas/showtimes?location={},{}&distance=5&&movie_id={}&time_from={}'.format(lat, lon, movie_id, time_from)
+
+        r = requests.get(showtime_request)
+        r_json = r.json()
+        print(r_json)
+        # TODO: Format the output
+
+        return r_json
+    else:
+        error = "An error has occurred: Invalid JSON input. Error code: {}".format(500)
+        return error
 
 @bp_mobile_api.route('/restaurants', methods=['POST'])
-def restaurants():
+def get_restaurants():
     if request.is_json:
         json_input = request.get_json()
         print(json_input)
