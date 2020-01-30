adc1fecbb85bacb031d4d373173be98034d9645d
Ayaz Ali Qureshi <a.qureshi@campus.tu-berlin.de>
2020-01-30 22:53:52 +0100

    Fetching list of genres from cinema api

--- api_webapp/flaskr/api/mobile.py
+++ api_webapp/flaskr/api/mobile.py
@@ -1,6 +1,6 @@
 import requests
 import datetime
-from flask import Blueprint, request
+from flask import Blueprint, request, session
 
 bp_mobile_api = Blueprint('mobile', __name__, url_prefix='/api/mobile')
 cinema_api_key = "EDX4mJjrvtFwWe0ZvrZ9NPwRITLTdjuA"
@@ -14,6 +14,9 @@ def movies():
         genre_preferences = json_input["GenrePreferences"]
         most_voted_genre = max(genre_preferences, key=lambda key: genre_preferences[key])
         print(most_voted_genre)
+        list_of_genres = _fetch_genres()
+        genre_obj = next((item for item in list_of_genres['genres'] if item['name'] == most_voted_genre), None)
+        print(genre_obj)
         headers = {"Content-type": "application/json"}
         return json_input
     else:
@@ -29,8 +32,14 @@ def restaurants():
         cuisine_preferences = json_input["CuisinePreferences"]
         most_voted_cuisine = max(cuisine_preferences, key=lambda key: cuisine_preferences[key])
         print(most_voted_cuisine)
+        print(g.list_of_genres)
         headers = {"Content-type": "application/json"}
         return json_input
     else:
         error = "An error has occurred: Invalid JSON input. Error code: {}".format(500)
         return error
+
+
+def _fetch_genres():
+    resp_list_of_genres = requests.get('http://localhost:5000/api/cinemas/list_of_genres')
+    return resp_list_of_genres.json()
