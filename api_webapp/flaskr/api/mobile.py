c32552f254994e90e0b026e5d0c0b761f749c0e9
Ayaz Ali Qureshi <a.qureshi@campus.tu-berlin.de>
2020-01-30 21:56:48 +0100

    Adding function for cuisine

--- api_webapp/flaskr/api/mobile.py
+++ api_webapp/flaskr/api/mobile.py
@@ -7,12 +7,27 @@ cinema_api_key = "EDX4mJjrvtFwWe0ZvrZ9NPwRITLTdjuA"
 
 
 @bp_mobile_api.route('/movies', methods=['POST'])
-def cinemas():
+def movies():
     if request.is_json:
         json_input = request.get_json()
         print(json_input)
         genre_preferences = json_input["GenrePreferences"]
-        most_voted_cuisine = max(genre_preferences, key=lambda key: genre_preferences[key])
+        most_voted_genre = max(genre_preferences, key=lambda key: genre_preferences[key])
+        print(most_voted_genre)
+        headers = {"Content-type": "application/json"}
+        return json_input
+    else:
+        error = "An error has occurred: Invalid JSON input. Error code: {}".format(500)
+        return error
+
+
+@bp_mobile_api.route('/restaurants', methods=['POST'])
+def restaurants():
+    if request.is_json:
+        json_input = request.get_json()
+        print(json_input)
+        cuisine_preferences = json_input["CuisinePreferences"]
+        most_voted_cuisine = max(cuisine_preferences, key=lambda key: cuisine_preferences[key])
         print(most_voted_cuisine)
         headers = {"Content-type": "application/json"}
         return json_input
